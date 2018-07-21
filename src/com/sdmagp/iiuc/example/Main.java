b577d38866604759f51cdcc300cbf20411befc4b
HP ProDesk <mehedipy@gmail.com>
2018-07-21 01:34:12 -0700

    second commit: added a new println()

--- src/com/sdmagp/iiuc/example/Main.java
+++ src/com/sdmagp/iiuc/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("hello from github!");
+        System.out.println("This is my second commit!");
+
     }
 }
